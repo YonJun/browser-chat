76eff5bcdea72a24aa5ebad5671a5fcadd922195
YonJun <jhon_08_2007@hotmail.com>
2022-01-17 23:05:02 -0500

    fix: welcome page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import { APP_NAME } from "../constants/app";
 
 export function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <Box
+      p="var(--spacing--3)"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+      height="100%">
+      <Typography variant="h1">{APP_NAME}</Typography>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/892/892749.png"
+        width="100%"
+        height="auto"
+        style={{ display: "block", maxWidth: "300px" }}
+      />
+    </Box>
+  );
 }
